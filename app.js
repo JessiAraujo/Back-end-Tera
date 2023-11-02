f941c4f95460f7498ba2de9a86cf8388734beb91
Jessica Araujo <jessii.aaraujo@gmail.com>
2023-11-02 14:01:53 -0300

    configuracoes de rotas

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 import bcrypt from "bcrypt";
-import multer from 'multer';
-import jwt from "jsonwebtoken"
+import multer from "multer";
+import jwt from "jsonwebtoken";
 
-const SECRET = '564581565468126';
+const SECRET = "564581565468126";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
@@ -17,116 +16,151 @@ const port = 4000;
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use('/uploads', express.static('uploads'));
-app.set('view engine', 'ejs');
-
+app.use(express.static("views"));
+app.use("/uploads", express.static("uploads"));
+app.set("view engine", "ejs");
 
 mongoose.connect(
-    'mongodb+srv://jessiiaaraujo:P0oWd6wj1QyDs4zl@cluster0.bq93zt3.mongodb.net/users', 
-{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  "mongodb+srv://jessiiaaraujo:P0oWd6wj1QyDs4zl@cluster0.bq93zt3.mongodb.net/users",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const Usuario = mongoose.model("Usuario", {
+  nome: String,
+  email: String,
+  senha: String,
+  foto: String,
 });
 
+app.get("/cadastro.html", (req, res) => {
+  res.sendFile(__dirname + "/views/cadastro.html");
+});
 
-const Usuario = mongoose.model('Usuario', {
-    nome: String,
-    email: String,
-    senha: String,
-    foto: String,
-  });
-
-
-app.get('/cadastro.html', (req, res) => {
-      res.sendFile(__dirname + '/views/cadastro.html');
-    });
-
-
-
-
-
-app.post('/cadastro', async (req, res) => {
+app.post("/cadastro", async (req, res) => {
   const { nome, email, senha } = req.body;
 
   try {
-    const hashedPassword = await bcrypt.hash(req.body.senha, 10); 
+    const hashedPassword = await bcrypt.hash(req.body.senha, 10);
 
     const novoUsuario = new Usuario({ nome, email, senha: hashedPassword });
     await novoUsuario.save();
-  console.log("teste");
+    res.cookie("nomeUsuario", nome, { httpOnly: true });
+    res.redirect(`/login?nome=${nome}`);
 
-    res.redirect('/login');
-
+    // res.redirect("/login");
   } catch (error) {
     console.error(error);
     return res.status(500).send("Erro ao cadastrar o usuário.");
+
   }
 });
 
-
-
-app.get('/usuarios', async (req, res) => {
+app.get("/usuarios", async (req, res) => {
   try {
-    const usuarios = await Usuario.find(); 
-   
-    res.json(usuarios); 
+    const usuarios = await Usuario.find();
+
+    res.json(usuarios);
   } catch (error) {
     console.error(error);
     return res.status(500).send("Erro ao buscar usuários.");
   }
 });
 
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
+});
 
-app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/views/login.html');
-  });
-
-  
-
- 
-
-  app.post('/login', async (req, res) => {
-    const { email, senha } = req.body;
+app.post("/login", async (req, res) => {
+  const { email, senha } = req.body;
 
   try {
     const usuario = await Usuario.findOne({ email });
 
     if (!usuario) {
-      return res.status(401).send('Usuário não encontrado');
+      return res.status(401).send("Usuário não encontrado");
     }
 
     const senhaValida = await bcrypt.compare(senha, usuario.senha);
 
     if (!senhaValida) {
-      return res.status(401).send('Senha incorreta');
+      return res.status(401).send("Senha incorreta");
     }
 
     // Se o login for bem-sucedido, gera um token JWT
-    const token = jwt.sign({ id: usuario._id }, SECRET, { expiresIn: '5s' });
+    const token = jwt.sign({ id: usuario._id }, SECRET, { expiresIn: "300s" });
 
     // Define o token como um cookie
-    res.cookie('token', token, { httpOnly: true });
+    res.cookie("token", token, { httpOnly: true });
 
-    console.log('Antes do redirecionamento para /perfil então até aqui está ok');
+    console.log(
+      "Antes do redirecionamento para /perfil então até aqui está ok"
+    );
 
-    res.redirect('/perfil');
+    res.redirect("/perfil");
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Erro ao fazer login.");
+  }
+});
 
+app.use("/uploads", express.static("uploads"));
+
+app.get("/perfil", verificaToken, (req, res) => {
+  app.get("/perfil", verificaToken, async (req, res) => {
+    try {
+      const usuario = await Usuario.findById(req.userId);
+    
+      if (!usuario) {
+        return res.status(404).send("Usuário não encontrado");
+      }
+  
+      const nomeUsuario = req.query.nome || usuario.nome;
+      const fotoUsuario = req.query.foto || usuario.foto;
+  
+      res.render(__dirname + "/views/perfil.html", { nomeUsuario, fotoUsuario });
+  
+
+  
     } catch (error) {
       console.error(error);
-      return res.status(500).send("Erro ao fazer login.");
+      return res.status(500).send("Erro ao carregar perfil.");
     }
   });
- 
+  
+  console.log("para verificar se perfil esta ok");
+
+  res.sendFile(__dirname + "/views/perfil.html");
+});
+
+app.post("/perfil", (req, res) => {});
+
+/
+app.get("/escolha", (req, res) => {
+  res.sendFile(__dirname + "/views/escolha.html");
+});
 
 
-    
-    
-// Middleware para verificar o token JWT
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads"); // Pasta onde as imagens serão salvas
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+
 function verificaToken(req, res, next) {
   const token = req.cookies.token;
 
   if (!token) {
+    console.log("Token não encontrado, redirecionando para /escolha");
+    res.clearCookie("token");
     return res.status(401).json({
       message: "Token não fornecido",
     });
@@ -134,11 +168,13 @@ function verificaToken(req, res, next) {
 
   jwt.verify(token, SECRET, (err, decoded) => {
     if (err) {
-      if (err.name === 'TokenExpiredError') {
-        // Token expirado, redirecionar para a página de login
-        return res.redirect('/login');
+      if (err.name === "TokenExpiredError") {
+        console.log("Token expirado, redirecionando para /escolha");
+        res.clearCookie("token");
+        return res.status(401).json({
+          message: "Token expirado",
+        });
       } else {
-        // Outro erro ao verificar o token
         return res.status(403).json({
           message: "Token inválido",
         });
@@ -151,82 +187,50 @@ function verificaToken(req, res, next) {
 }
 
 
-
-app.use('/uploads', express.static('uploads'));
-
-   
-// app.get('/perfil', verificaToken, (req, res) => {
-//   // vai poder acessar req.userId - req.id para saber qual usuário está logado
-//   res.sendFile(__dirname + '/views/perfil.html');
-// });
-    
-app.get('/perfil',verificaToken, (req, res) => {
-  //prossegue se o token for válido
-  //cookie incluido no redirecionamento pro perfil
-  // da p acessar req.userId - req.id para saber qual usuário está logado
-  res.sendFile(__dirname + '/views/perfil.html');
+app.get("/configuracao", (req, res) => {
+  res.sendFile(__dirname + "/views/configuracao.html");
 });
 
+app.post("/atualizar-perfil", verificaToken, async (req, res) => {
+  const { nome } = req.body;
 
-
-app.post('/perfil', (req, res) => {
-
-});
-
-   // essa rota é pra exibir a escolha do usuário
-   app.get('/escolha', verificaToken, (req, res) => {
-    // apos expirar escolher continuar logado ou fazer login novamente
-    res.sendFile(__dirname + '/views/escolha.html');
-  });
-
-  
-
-app.get('/configuracao', (req, res) => {
-  res.sendFile(__dirname + '/views/configuracao.html');
-});
-
-// Configuração do multer para salvar as imagens em um diretório no servidor
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads'); // Pasta onde as imagens serão salvas
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname); 
-  },
-});
-
-const upload = multer({ storage: storage });
-
-/// Rota para processar o upload da nova foto de perfil
-app.post('/perfil', verificaToken, upload.single('foto'), async (req, res) => {
   try {
-    // Verificar se houve um erro durante o upload
-    if (req.fileValidationError) {
-      return res.status(400).send('Erro no upload da imagem: ' + req.fileValidationError);
-    } else if (!req.file) {
-      return res.status(400).send('Nenhuma imagem foi selecionada.');
-    }
-
-    // O upload foi bem-sucedido, você pode salvar o caminho do arquivo no banco de dados
-    const filePath = req.file.path;
-
-    // Atualizar a propriedade 'foto' no usuário com o novo caminho do arquivo
-    await Usuario.findByIdAndUpdate(req.userId, { foto: filePath });
-
-    // Redirecionar de volta para a página de perfil
-    res.redirect('/perfil');
-    // res.send('<script>window.location.href = "/perfil";</script>');
-
+    await Usuario.findByIdAndUpdate(req.userId, { nome });
+    res.redirect("/perfil");
   } catch (error) {
     console.error(error);
-    return res.status(500).send("Erro ao salvar a nova foto de perfil.");
+    return res.status(500).send("Erro ao atualizar o perfil.");
   }
 });
 
+
+app.post(
+  "/atualizar-foto",
+  verificaToken,
+  upload.single("foto"),
+  async (req, res) => {
+    try {
+      if (req.fileValidationError) {
+        return res
+          .status(400)
+          .send("Erro no upload da imagem: " + req.fileValidationError);
+      } else if (!req.file) {
+        return res.status(400).send("Nenhuma imagem foi selecionada.");
+      }
+
+      const filePath = req.file.path;
+      await Usuario.findByIdAndUpdate(req.userId, { foto: filePath });
+
+      res.redirect("/perfil");
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send("Erro ao salvar a nova foto de perfil.");
+    }
+  }
+);
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
 
 export default app;
-
